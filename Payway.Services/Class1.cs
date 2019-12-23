950b7719c139e2d38ea2f3b959a0f10ae0c30005
cshar <cshar@DESKTOP-GSLJUQM>
2019-12-23 21:15:07 +0000

    EmployeeServices

--- Payway.Services/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Payway.Services
-{
-    public class Class1
-    {
-    }
-}
--- Payway.Services/IEmployeeService.cs
+++ Payway.Services/IEmployeeService.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
-using Payway.Entity
+using Payway.Entity;
 
 namespace Payway.Services
 {
     public interface IEmployeeService
     {
         Task CreateAsync(Employee newEmployee);
-        Employee GetById(int )
+        Employee GetById(int employeeId);
+        Task UpdateAsync(Employee employee);
+        Task UpdateAsync(int id);
+        Task Delete(int employeeId);
+
         
     }
 }
