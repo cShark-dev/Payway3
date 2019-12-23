74b92a58a59bb69a748c1090d3cf4c8a71b28861
cshar <cshar@DESKTOP-GSLJUQM>
2019-12-23 21:31:56 +0000

    Index - EmployeeController

--- Payway.Services/IEmployeeService.cs
+++ Payway.Services/IEmployeeService.cs
@@ -6,14 +6,15 @@ using Payway.Entity;
 
 namespace Payway.Services
 {
-    public interface IEmployeeService
+    public interface IEmployeeService   
     {
-        Task CreateAsync(Employee newEmployee);
-        Employee GetById(int employeeId);
-        Task UpdateAsync(Employee employee);
-        Task UpdateAsync(int id);
-        Task Delete(int employeeId);
-
-        
+        Task CreateAsync(Employee newEmployee);   //to create an employee, we create a new employee called newEmployee
+        Employee GetById(int employeeId);  //A method to Retrieve employees by ID, we pass in the employee ID
+        Task UpdateAsync(Employee employee); //A method to update the employees record We need to pass in an employee
+        Task UpdateAsync(int id); //Update an employee by ID, the method signature is different, a method can have the same name as long they have different signatures
+        Task Delete(int employeeId); //A method to delete, you usually delete using a unique identifier, we 
+        decimal UnionFees(int id); //Retrieve a union fee, first pass the employee id t ocheck if the employee is a union member, if member then a union fee will apply to this employee 
+        decimal StudentLoanRepaymentAmount(int id, decimal totalAmount); //We check if the employee is paying back student loan, we need to know the total amount the employee has earned
+        IEnumerable<Employee> GetAll(); //A method to return a list of all the employees, an IEnumerable of employees, a collection of employees that we call GetAll
     }
 }
