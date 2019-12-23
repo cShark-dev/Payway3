1d9c2fc412c78227408132577a3e0b1da536512b
cshar <cshar@DESKTOP-GSLJUQM>
2019-12-23 22:25:46 +0000

    Updated EmployeeController

---
+++ Payway/Models/EmployeeCreateViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Payway.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payway.Models
+{
+    public class EmployeeCreateViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Employee Number is required"),
+            RegularExpression(@"^[A-Z]{3, 3}[0-9]{3}$")]
+        public string EmployeeNo { get; set; }
+        [Required(ErrorMessage = "First Name is required"), StringLength(50, MinimumLength =2)]
+        [RegularExpression(@"^[A-Z][a-zA-Z""\s-]*$"), Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [StringLength(50), Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+        [Required(ErrorMessage = "Last Name is required"), StringLength(50, MinimumLength = 2)]
+        [RegularExpression(@"^[A-Z][a-zA-Z""\s-]*$"), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        public string FullName {   
+           get  {
+                return FirstName + (string.IsNullOrEmpty(MiddleName) ? " " : (" " + (char?)MiddleName[0] + ".").ToUpper()) + LastName;
+           }          
+        }
+        public string Gender { get; set; }
+        [Display(Name = "Photo")]
+        public IFormFile ImageUrl { get; set; }
+        [DataType(DataType.Date), Display(Name = "Date of Birth")]
+        public DateTime DOB { get; set; }
+        [DataType(DataType.Date), Display(Name = "Date Joined")]
+        public DateTime DateJoined { get; set; }
+        public string Designation { get; set; }
+
+        public string Email { get; set; }
+        [Required, MaxLength(50)]
+        public string NationalInsuranceNo { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+        public StudentLoan StudentLoan { get; set; }
+        public UnionMember UnionMember { get; set; }
+        [Required,MaxLength(50)]
+        public string Postcode { get; set; }
+
+    }
+}
