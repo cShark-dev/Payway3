caf8d790bcaaaf27d644d96368e08c8f2ba582aa
cshar <cshar@DESKTOP-GSLJUQM>
2020-01-13 23:45:50 +0000

    Create.cshtml in Views linked to EmployeeCreateViewModel

--- Payway/Models/EmployeeCreateViewModel.cs
+++ Payway/Models/EmployeeCreateViewModel.cs
@@ -12,7 +12,7 @@ namespace Payway.Models
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Employee Number is required"),
-            RegularExpression(@"^[A-Z]{3, 3}[0-9]{3}$")]
+            RegularExpression(@"^[A-Z]{3,3}[0-9]{3}$")]
         public string EmployeeNo { get; set; }
         [Required(ErrorMessage = "First Name is required"), StringLength(50, MinimumLength =2)]
         [RegularExpression(@"^[A-Z][a-zA-Z""\s-]*$"), Display(Name ="First Name")]
@@ -43,7 +43,6 @@ namespace Payway.Models
         public string Email { get; set; }
                  
         [Required, StringLength(50), Display(Name = "NI No. ")]
-        [RegularExpression(@"^[A-CEGHJ-PR-TW-Z] {1} [A-CEGHJ-NPR-TW-Z] {1} [0-9] {6} [A-D\s]$")]
         public string NationalInsuranceNo { get; set; }
         [Display(Name ="Payment Method")]
         public PaymentMethod PaymentMethod { get; set; }
