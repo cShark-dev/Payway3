c73b5fb8dcfa98eaabd45501e947e92c97df30a6
cshark-dev <asteroidzeta@gmail.com>
2020-01-25 22:16:03 +0000

    25012020 taxService - Interface and Implementation

---
+++ Payway.Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payway.Services
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);     //the tax amount depends on the totalAmount we earn
+    }
+}
